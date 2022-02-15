a06434126d348f1ee1422caf7fab6ef43ca7ef56
pranavp7 <85606269+pranavp7@users.noreply.github.com>
2022-02-15 19:36:53 +0530

    Add files via upload

---
+++ Square_root.py
@@ -0,0 +1,7 @@
+#Python code to calculate the square root of a number taken from user input
+import math
+num = int(input("Enter a number : "))
+
+sqRoot = math.sqrt(num)
+
+print(f"The square root of {num} is :",sqRoot)
